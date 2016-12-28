f66b5c01a35cf6c2717e9daea2389a5e1df9f9e2
slashhuang <627284482@qq.com>
2016-12-28 16:12:20 +0800

    set to new repo koa-rudy

---
+++ app/resources/category.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: enzo
+ * @Date:   2016-11-10 10:20:28
+ * @Last Modified by:   enzo
+ * @Last Modified time: 2016-11-11 16:31:09
+ */
+
+module.exports = [
+
+    {
+        title: 'get detail v1',
+        url: '/:id',
+        version: 'v1',
+        action: async function(ctx, next) {
+            await ctx.render('index', {
+                title: 'koa-rudy'
+            });
+        }
+    },
+
+    {
+        title: 'get detail v2',
+        doc: '',
+        method: 'get',
+        version: 'v2',
+        url: '/:id',
+        action: async function(ctx, next) {
+            await ctx.render('index', {
+                title: 'koa-rudy'
+            });
+        }
+    }
+
+]
