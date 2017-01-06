4418e92d4e7d11012a59aa193f946e6d0dad8553
Enzo <enzoyang@superjia.com>
2017-01-06 11:29:45 +0800

    dev babel register

--- server.js
+++ server.js
@@ -3,7 +3,10 @@
  * 主程序入口
  * 16.12.23
  */
-require("babel-register");
+
+if (process.env.NODE_ENV == 'dev') {
+    require("babel-register");
+}
 
 const debug = require('debug')('rudy:server');
 
